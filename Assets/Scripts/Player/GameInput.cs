2d6e0ba855972196e5d6469ece779b591f52d14a
洪子佳 <1577581417@qq.com>
2024-01-15 15:05:44 +0800

    [updata]使用射线检测实现人物碰撞

--- Assets/Scripts/Player/GameInput.cs
+++ Assets/Scripts/Player/GameInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using ZFramework;
 public class GameInput : SingletonMono<GameInput>
@@ -10,7 +11,15 @@ public class GameInput : SingletonMono<GameInput>
         base.Awake();
         playerInputActions = new PlayerInputAction();
         playerInputActions.Player.Enable();
+        //当指定的按键被按下时触发
+        playerInputActions.Player.Interact.performed += Interact_performed;
     }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        Debug.Log(obj);
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,20 +5,80 @@ using DG.Tweening;
 using ZFramework;
 public class Player : SingletonMono<Player>
 {
-    public float speed;
+    public float moveSpeed;
     public float rotateSpeed;
+    public float playerRadius = 2.7f;
+    public float playerHeight = 2.7f;
+    public LayerMask countLayeMask;
     private bool isWarking;
+    private Vector3 lastInteractDir;
     void Update()
     {
-        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * speed * Time.deltaTime;
-        isWarking = moveDir != Vector3.zero;
-        transform.forward = Vector3.Lerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+        HandleMovement();
+        HandleInterctions();
     }
 
     public bool IsWarking()
     {
         return isWarking;
+    }
+
+    private void HandleInterctions()
+    {
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        if (moveDir != Vector3.zero)
+            lastInteractDir = moveDir;
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycast, interactDistance, countLayeMask))
+        {
+            //检查是否携带某个控件
+            if (raycast.transform.TryGetComponent<ClearCounter>(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+
+        }
+
+    }
+    private void HandleMovement()
+    {
+
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        //球形碰撞射线或者胶囊体碰撞射线(位置（低点），位置（高点）,半径，方向，移动距离)
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position
+            + Vector3.up * playerRadius, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            //尝试单个轴向是否可以移动
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position
+           + Vector3.up * playerRadius, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //尝试单个轴向是否可以移动
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position
+               + Vector3.up * playerRadius, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        isWarking = moveDir != Vector3.zero;
+
+        transform.forward = Vector3.Lerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
 }
